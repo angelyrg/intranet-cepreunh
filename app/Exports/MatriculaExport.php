d183044fde9795ff1a1d0bdc4d16dbed2cce85c2
Angel YG <angel.yarang4@gmail.com>
2024-12-24 06:56:53 -0500

    Add export in Excel

---
+++ app/Exports/MatriculaExport.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Intranet\Matricula;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Illuminate\Support\Facades\DB;
+
+class MatriculaExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize
+{
+    protected $matriculas;
+
+    // Recibe las matrículas seleccionadas
+    public function __construct($matriculas)
+    {
+        $this->matriculas = $matriculas;
+    }
+
+    // Devuelve la colección de matrículas, que ya debe estar filtrada
+    public function collection()
+    {
+        return $this->matriculas;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'DNI', 
+            'Nombres', 
+            'Apellido Paterno', 
+            'Apellido Materno', 
+            'Género', 
+            'Carrera', 
+            'Área', 
+            'Sede', 
+            'Modalidad de Estudio', 
+            'Condición Académica', 
+            'Tel. Personal', 
+            'Whatsapp', 
+            'Correo Personal', 
+            'Correo Institucional', 
+            '¿Tiene Discapacidad?', 
+            'Modalidad Matrícula', 
+            'Fecha de Matrícula'
+        ];
+    }
+
+    public function map($matricula): array
+    {
+        return [
+            $matricula->estudiante->nro_documento,
+            $matricula->estudiante->nombres,
+            $matricula->estudiante->apellido_paterno,
+            $matricula->estudiante->apellido_materno,
+            $matricula->estudiante->genero->descripcion,
+            $matricula->carrera->descripcion,
+            $matricula->area->descripcion,
+            $matricula->sede->descripcion,
+            $matricula->modalidad_estudio,
+            $matricula->condicion_academica,
+            $matricula->estudiante->telefono_personal,
+            $matricula->estudiante->whatsapp,
+            $matricula->estudiante->correo_personal,
+            $matricula->estudiante->correo_institucional,
+            $matricula->estudiante->tiene_discapacidad ? 'Sí' : 'No',
+            $matricula->modalidad_matricula == 1 ? 'Presencial' : ($matricula->modalidad_matricula == 2 ? 'Virtual' : 'Desconocido'),
+            $matricula->created_at->format('d/m/Y h:i:sA')
+        ];
+    }
+}
