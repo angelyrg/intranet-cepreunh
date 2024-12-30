9375e22048a1ccb892daf3f3896616e077f0803c
Angel YG <angel.yarang4@gmail.com>
2024-12-30 16:07:47 -0500

    Add links to ciclo activos in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,4 +28,30 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-12 mb-3">
+            <div class="bg-primary-subtle rounded-top-2 px-3 py-2">
+                <h5 class="text-primary mb-0">Ciclos activos</h5>
+            </div>
+        </div>
+        @foreach ($ciclos as $ciclo)   
+            <div class="col-12 col-md-6">
+                <div class="card">
+                    <div class="card-body p-4 d-flex align-items-center gap-3">
+                        <div>
+                            <h5 class="fw-semibold mb-0">{{ $ciclo->descripcion }}</h5>
+                            <span class="fs-2 d-flex align-items-center">
+                                <i class="ti ti-calendar text-dark fs-3 me-1"></i>
+                                <span>{{ \Carbon\Carbon::parse($ciclo->created_at)->format('d/m/Y') }}</span>
+                            </span>
+                        </div>
+                        <a href="{{ route('ciclos.show', $ciclo->id) }}" class="btn btn-primary py-1 px-2 ms-auto">
+                            Ver
+                            <i class="ti ti-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
